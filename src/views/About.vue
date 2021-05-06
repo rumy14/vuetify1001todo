2c53edcc52a7b625b68143f26ebb08c2c9729005
rumy14 <unitedit21@gmail.com>
2021-05-06 23:22:38 +0600

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nostrum incidunt odio quis, 
+      ab vel explicabo commodi beatae porro rem similique ad provident. Enim, dolor minus 
+      illum provident praesentium corrupti.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nostrum incidunt odio quis, 
+      ab vel explicabo commodi beatae porro rem similique ad provident. Enim, dolor minus 
+      illum provident praesentium corrupti.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nostrum incidunt odio quis, 
+      ab vel explicabo commodi beatae porro rem similique ad provident. Enim, dolor minus 
+      illum provident praesentium corrupti.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nostrum incidunt odio quis, 
+      ab vel explicabo commodi beatae porro rem similique ad provident. Enim, dolor minus 
+      illum provident praesentium corrupti.
+    </p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pa-6">
+    <h1>Todo Page</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '',
+  }
+</script>
